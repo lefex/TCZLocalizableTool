0e5b21ff962b075d863dd483f99f9dcb0cee584d
wangsuyan <wsyxyxs@126.com>
2017-11-08 17:25:32 +0800

    检查未使用的图片，添加 xib 和 storyboard 的支持

--- LocalToos/unUseImage.py
+++ LocalToos/unUseImage.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# coding=utf-8 
 
 import os
 import re
@@ -8,8 +8,8 @@ import shutil
 # 将自动被删除。建议确认所有的图没用后开启
 IS_OPEN_AUTO_DEL = False
 
-# 将要解析的项目名称
-DESPATH = "/Users/wangsuyan/desktop/project/ShopnFriends/ShopnFriends"
+# 将要解析的项目名称 
+DESPATH = "/Users/wangsuyan/Desktop/project/Kmart"
 
 # 可能检查出错的图片，需要特别留意下
 ERROR_DESPATH = "/Users/wangsuyan/Desktop/unUseImage/error.log"
@@ -22,16 +22,14 @@ IMAGE_WDESPATH = "/Users/wangsuyan/Desktop/unUseImage/images/"
 
 # 目录黑名单，这个目录下所有的图片将被忽略
 BLACK_DIR_LIST = [
-                  DESPATH + '/ThirdPart', # Utils 下所有的文件将被忽略
-                  ]
+    DESPATH + '/ThirdPart', # Utils 下所有的文件将被忽略 
+]
 
 # 已知某些图片确实存在，比如像下面的图，脚本不会自动检查出，需要手动加入这个数组中
 # NSString *name = [NSString stringWithFormat:@"loading_%d",i];
 # UIImage *image = [UIImage imageNamed:name];
 EXCEPT_IMAGES = [
-                 'loading_',
-                 'launch-guide'
-                 ]
+]
 
 # 项目中所有的图
 source_images = dict()
@@ -68,22 +66,22 @@ def is_except_image(filePath):
     return False
 
 def auto_remove_images():
-    with open(WDESPATH, 'r') as f:
-        for line in f.readlines():
-            path = DESPATH + line.strip('\n')
-            if not os.path.isdir(path):
-                if 'Assets.xcassets' in line:
-                    path = os.path.split(path)[0]
-                    if os.path.exists(path):
-                        shutil.rmtree(path)
-                else:
-                    os.remove(path)
+    f = open(WDESPATH, 'r')
+    for line in f.readlines():
+        path = DESPATH + line.strip('\n')
+        if not os.path.isdir(path):
+            if 'Assets.xcassets' in line:
+                path = os.path.split(path)[0]
+                if os.path.exists(path):
+                    shutil.rmtree(path)
+            else:
+                os.remove(path)
 
 
 def un_use_image(filePath):
     if re.search(r'\w@3x.(png|jpg|jpeg|gif)', filePath):
         return
-    
+
     if re.search(r'\w(@2x){0,1}.(png|jpg|jpeg|gif)', filePath):
         exts = os.path.splitext(filePath)
         result = (filename(filePath).replace('@2x', '')).replace(exts[1],'')
@@ -93,15 +91,21 @@ def find_image_name(filePath):
     f = open(filePath)
     for index, line in enumerate(f):
         line = line.strip()
-        regx = r'\[\s*UIImage\s+imageNamed\s*:\s*@"(.+?)"'
+
+        if '.xib' in filePath or 'storyboard' in filePath:
+            regx = r'image="(.+?).(?:png|jpg|jpeg|gif)"'
+        else:
+            regx = r'\[\s*UIImage\s+imageNamed\s*:\s*@"(.+?)"'
         matchs = re.findall(regx, line)
         if matchs:
             for item in matchs:
+                print(item)
                 use_images.add(item)
         else:
             err_matchs = re.findall(r'\[UIImage imageNamed:', line)
             if err_matchs:
                 name = filename(filePath)
+                # print(line)
                 for item in err_matchs:
                     err_images.add(str(index + 1) + ':' + name + '\n' + line + '\n')
 
@@ -109,6 +113,7 @@ def find_from_file(path):
     paths = os.listdir(path)
     for aCompent in paths:
         aPath = os.path.join(path, aCompent)
+        ext = os.path.splitext(aPath)[1]
         if isInBlackList(aPath):
             print('在黑名单中，被自动忽略' + aPath)
             continue
@@ -116,7 +121,7 @@ def find_from_file(path):
             find_from_file(aPath)
         elif os.path.isfile(aPath) and isimage(aPath) and not isappicon(aPath) and not is_except_image(aPath):
             un_use_image(aPath)
-        elif os.path.isfile(aPath) and os.path.splitext(aPath)[1]=='.m':
+        elif os.path.isfile(aPath) and (ext=='.m' or ext=='.xib' or ext=='.storyboard'):
             find_image_name(aPath)
 
 if __name__ == '__main__':
@@ -125,17 +130,20 @@ if __name__ == '__main__':
 
     os.makedirs(IMAGE_WDESPATH)
 
-with open(WDESPATH, 'w') as wf:
+    wf = open(WDESPATH, 'w')
     find_from_file(DESPATH)
     for item in set(source_images.keys()) - use_images:
         value = source_images[item]
         wf.write(value.replace(DESPATH, '') + '\n')
         ext = os.path.splitext(value)[1]
         shutil.copyfile(value, IMAGE_WDESPATH + item + ext)
-        
-        with open(ERROR_DESPATH, 'w') as ef:
-            for item in err_images:
-                ef.write(item)
 
-if IS_OPEN_AUTO_DEL:
-    auto_remove_images()
+    wf.close()
+
+    ef = open(ERROR_DESPATH, 'w')
+    for item in err_images:
+        ef.write(item)
+    ef.close()
+
+    if IS_OPEN_AUTO_DEL:
+        auto_remove_images()
